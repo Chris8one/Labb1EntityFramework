e8b36099a49a70b27cfef2dcfd48db27947c55e9
Chris Granbäck <chris.granback@edu.edugrade.se>
2022-08-25 11:15:44 +0200

    Add project files.

---
+++ Labb1EntityFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labb1EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
